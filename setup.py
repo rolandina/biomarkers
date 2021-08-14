c95b219ff9bda3ac5ce63e0da2661a9609b0305f
Smirnova Nina <ms.nina.smirnova@gmail.com>
2021-08-14 17:54:50 +0200

    initialize project + create notebook with fast data cleaning

---
+++ setup.py
@@ -0,0 +1,10 @@
+
+from setuptools import setup
+
+setup(
+    name="biomarkers",
+    version='0.0.1',
+    packages=["biomarkers"],
+    install_requires=['pandas', 'numpy', 'requests', 'setuptools'],
+    #scripts = ['scripts/some_script_name']
+    )
